9a25f6314e437e68b3e9d70dfde7347b86fddf6e
Saurabh <saurabhgurde@gmail.com>
2023-09-30 17:09:30 +0530

    2

--- Subspace Test/index.js
+++ Subspace Test/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import _ from 'lodash';
+import cors from 'cors';
 const PORT = 8000;
 import getBlogRoutes from "./routes/getBlogs.js"
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', getBlogRoutes);
